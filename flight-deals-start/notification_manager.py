af2b04baa7fb114c4d40683678d3473552ce3905
Ahmad Khalifeh <75331165+khalah3@users.noreply.github.com>
2024-09-22 21:49:22 -0500

    Update notification_manager.py

--- flight-deals-start/notification_manager.py
+++ flight-deals-start/notification_manager.py
@@ -1,3 +1,15 @@
+from smtplib import SMTP
+import os
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def __init__(self):
+        pass
+
+    #This code will initiate email with the message in main.py
+    def send_emails(self,message,destination_address):
+        with SMTP("smtp.gmail.com") as connection:
+            password = os.environ['gmail_connection_password']
+            connection.starttls()
+            connection.login(user='georgehuffingtons@gmail.com', password=password)
+            connection.sendmail(from_addr='georgehuffingtons@gmail.com', to_addrs=destination_address,msg=f"{message}")
